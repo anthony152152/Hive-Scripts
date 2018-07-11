4ce8776d6896424c87502b47d692c824e20a3bcd
anthony152152 <32080642+anthony152152@users.noreply.github.com>
2018-07-11 21:06:40 +0200

    Add files via upload

--- h-stats.sh
+++ h-stats.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-stats_raw=`curl --connect-timeout 2 --max-time $API_TIMEOUT --silent --noproxy '*' http://127.0.0.1:60050`
+stats_raw=`curl --connect-timeout 2 --silent --noproxy '*' http://127.0.0.1:60060`
 			#echo $stats_raw | jq .
 			if [[ $? -ne 0 || -z $stats_raw ]]; then
-				echo -e "${YELLOW}Failed to read $miner from localhost:60050${NOCOLOR}"
+				echo -e "${YELLOW}Failed to read $miner from localhost:60060${NOCOLOR}"
 			else
 				khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
 				local cpu_temp=`cat /sys/class/hwmon/hwmon0/temp*_input | head -n $(nproc) | awk '{print $1/1000}' | jq -rsc .` #just a try to get CPU temps
